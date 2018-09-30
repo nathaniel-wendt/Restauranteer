259aeecbfaa8950d1f3922c4a650e0185c0ea96e
nathaniel-wendt <nathaniel.wendt@gmail.com>
2018-09-30 11:09:16 -0600

    add MapBox API

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibmVvZ2VuZXRpY3MiLCJhIjoiY2ptcDNubGZoMWIydzNrcXRtZ2I4bjdzcCJ9.IqiiaWn9nrOuuGRR_sgQog',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
